0c79c136cea929848b9885d46ef4cfe5f37304d3
SALma-LoMy <salmametwalyelhenawy@gmail.com>
2024-12-26 04:30:13 +0200

    frist commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let togglestatus = true;
+document.addEventListener("DOMContentLoaded",(event)=>{
+    console.log("hi")
+    const p = document.querySelector("profil-img");
+    p.addEventListener("click",()=>{
+        if(togglestatus === false ){
+            document.querySelector(".drop-down").getElementsByClassName.visibility="visible";
+            document.querySelector("#home1").setAttribute("d", "M45.5 48H30.1c-.8 0-1.5-.7-1.5-1.5V34.2c0-2.6-2.1-4.6-4.6-4.6s-4.6 2.1-4.6 4.6v12.3c0 .8-.7 1.5-1.5 1.5H2.5c-.8 0-1.5-.7-1.5-1.5V23c0-.4.2-.8.4-1.1L22.9.4c.6-.6 1.6-.6 2.1 0l21.5 21.5c.3.3.4.7.4 1.1v23.5c.1.8-.6 1.5-1.4 1.5z");
+            togglestatus =true;
+        }
+        else if(togglestatus === true){
+            document.querySelector(".drop-down").getElementsByClassName.visibility="visible";
+            document.querySelector("#home1").setAttribute("d", "M45.5 48H30.1c-.8 0-1.5-.7-1.5-1.5V34.2c0-2.6-2.1-4.6-4.6-4.6s-4.6 2.1-4.6 4.6v12.3c0 .8-.7 1.5-1.5 1.5H2.5c-.8 0-1.5-.7-1.5-1.5V23c0-.4.2-.8.4-1.1L22.9.4c.6-.6 1.6-.6 2.1 0l21.5 21.5c.3.3.4.7.4 1.1v23.5c.1.8-.6 1.5-1.4 1.5z");
+            togglestatus = false;
+
+        };
+    });
+});
